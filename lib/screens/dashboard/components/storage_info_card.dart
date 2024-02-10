848d3f80ace16146529f3b194c180d45118ae88b
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-10 23:47:29 +0200

    StorageInfoCard

--- lib/screens/dashboard/components/storage_info_card.dart
+++ lib/screens/dashboard/components/storage_info_card.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
+
+class StorageInfoCard extends StatelessWidget {
+  const StorageInfoCard({
+    Key? key,
+  
