fbc091165a7b1a2ec59dc8ea07826eea4c28b1ab
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-11 00:06:26 +0200

    Is Tabler

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -18,3 +18,7 @@ class Responsive extends StatelessWidget {
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width < 850;
 
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1100 &&
+      MediaQuery.of(context).size.width >= 850;
+
