94ee9fa404880e791589477f81bebccbdb1c3bf2
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-10 23:49:09 +0200

    Mdels

--- lib/screens/dashboard/components/file_info_card.dart
+++ lib/screens/dashboard/components/file_info_card.dart
@@ -1,26 +1,2 @@
 import 'package:admin/models/MyFiles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../../constants.dart';
-
-class FileInfoCard extends StatelessWidget {
-  const FileInfoCard({
-    Key? key,
-    required this.info,
-  }) : super(key: key);
-
-  final CloudStorageInfo info;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(defaultPadding),
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
