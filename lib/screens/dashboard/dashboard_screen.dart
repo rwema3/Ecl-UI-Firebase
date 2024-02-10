b129ccc3b7aaf5b8c03ce37e2be56035811ba092
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-10 23:55:08 +0200

    fields and material

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,3 +1,7 @@
+import 'package:admin/responsive.dart';
+import 'package:admin/screens/dashboard/components/my_fields.dart';
+import 'package:flutter/material.dart';
+
 import '../../constants.dart';
 import 'components/header.dart';
 
