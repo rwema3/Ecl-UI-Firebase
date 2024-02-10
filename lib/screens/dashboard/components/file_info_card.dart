49387e41a552185fe6aa1a240073659613e61c8c
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-10 23:52:57 +0200

    Layout Builder

--- lib/screens/dashboard/components/file_info_card.dart
+++ lib/screens/dashboard/components/file_info_card.dart
@@ -100,4 +100,17 @@ class ProgressLine extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
         ),
-    
+        LayoutBuilder(
+          builder: (context, constraints) => Container(
+            width: constraints.maxWidth * (percentage! / 100),
+            height: 5,
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
