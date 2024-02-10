b6fb82931ec722db60d74ee7b31927820a36bd5b
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-11 00:06:34 +0200

    Is desktop

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -22,3 +22,6 @@ class Responsive extends StatelessWidget {
       MediaQuery.of(context).size.width < 1100 &&
       MediaQuery.of(context).size.width >= 850;
 
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1100;
+
