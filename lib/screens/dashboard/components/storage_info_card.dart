40aff2df979a2e6800777be14800fd327f754a26
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-10 23:47:36 +0200

    inputs

--- lib/screens/dashboard/components/storage_info_card.dart
+++ lib/screens/dashboard/components/storage_info_card.dart
@@ -6,4 +6,10 @@ import '../../../constants.dart';
 class StorageInfoCard extends StatelessWidget {
   const StorageInfoCard({
     Key? key,
+    required this.title,
+    required this.svgSrc,
+    required this.amountOfFiles,
+    required this.numOfFiles,
+  }) : super(key: key);
+
   
