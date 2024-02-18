2b4fc3010f00e9c77213a3b5ddaaacdb8d910c49
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-18 23:22:56 +0200

    materials import

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,4 @@
 import 'package:admin/constants.dart';
 import 'package:admin/controllers/MenuAppController.dart';
+import 'package:admin/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
