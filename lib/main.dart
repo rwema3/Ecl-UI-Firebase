a30c9674a79efe7e5e81ed8c1b2cf590f3386967
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-02-18 23:23:41 +0200

    banner disabling

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Admin Panel',
