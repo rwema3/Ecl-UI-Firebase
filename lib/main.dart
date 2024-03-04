99c15cdbfe565ba21b7df5a7e33751cb6d1b4f27
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-03-04 23:58:40 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // RPF ELECTORIAL SYSTEM
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
